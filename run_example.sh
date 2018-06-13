46d0f273dfecd005facbffb3fa500bec70cd5ce3
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2018-06-13 13:36:00 +0200

    remove binary output tests and backups

--- run_example.sh
+++ run_example.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 python3.5 deepcoil.py -i example/test.fasta  -out_path example/out_seq/
 python3.5 deepcoil.py -i example/test.fasta  -pssm -pssm_path example/ -out_path example/out_pssm
-
-python3.5 deepcoil.py -i example/test.fasta  -out_type h5 -out_filename example/out_seq/GCN4_YEAST.h5
-python3.5 deepcoil.py -i example/test.fasta  -pssm -pssm_path example/ -out_type h5 -out_filename example/out_pssm/GCN4_YEAST.h5
